95c0ee0eeb99c1d1e666ea13c15509919a05a50b
Guilherme <guilherme.tarter@kinamic.com>
2023-11-13 18:33:42 -0300

    Update environments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {}
 };
+
+export const CLOUD_FUNCTION_URL = '';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,28 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyD_u_thQhFoi_WjcCSeKchJE5tIJ1bPh-A",
-    authDomain: "personal-aide.firebaseapp.com",
-    projectId: "personal-aide",
-    storageBucket: "personal-aide.appspot.com",
-    messagingSenderId: "625673028062",
-    appId: "1:625673028062:web:40a97612e5411a3d639d4f",
-    measurementId: "G-N8B02D34PW"
-  }
+  firebaseConfig: {}
 };
 
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
-
-export const HUGGING_FACE_API_TOKEN = "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";
-export const HUGGING_FACE_API_URL = 'https://api-inference.huggingface.co/models';
+export const CLOUD_FUNCTION_URL = '';
